a568b2ae1a4d8e3e88d5658b4e3f5dc2968b716a
DasHonmunuo <124122787+DasHonmunuo@users.noreply.github.com>
2024-03-28 23:16:31 +0800

    improved register and added login

--- Classes/User.php
+++ Classes/User.php
@@ -1,5 +1,6 @@
 <?php
     include("database.php");
+    session_start();
 
 class User {
     private $username;
@@ -12,23 +13,42 @@ class User {
 
     public function register(){
         global $conn; // Declare $conn as global
-        if(empty($this->username)){
-            echo"Please enter a username";
-        }
-        elseif(empty($this->password)){
-            echo"Please enter a password";
-        }
-        else{
-            $hash = password_hash($this->password, PASSWORD_DEFAULT);
+        
+        $hash = password_hash($this->password, PASSWORD_DEFAULT);
 
-            $sql = "INSERT INTO users (user,password) VALUES ('$this->username','$hash')";
-            try{
-                mysqli_query($conn, $sql);
-                echo"You are now registered";
-            }
-            catch(mysqli_sql_exception){
-                echo"Could not register user";
-            }
+        $sql = "INSERT INTO users (user,password) VALUES ('$this->username','$hash')";
+
+        try{
+            mysqli_query($conn, $sql);
+            echo"You are now registered";
+
+            $_SESSION["username"] = $this->username;
+
+            header("Location: index.html");
         }
+        catch(mysqli_sql_exception){
+            echo"Could not register user";
+        }
+    }
+
+    public function login(){
+        global $conn;
+        $sql = "SELECT * FROM users WHERE user = '$this->username'";
+        $result = mysqli_query($conn, $sql);
+
+            if(mysqli_num_rows($result)>0){
+                $row = mysqli_fetch_assoc($result);
+                if(password_verify($this->password, $row["password"])){
+                    $_SESSION["username"] = $this->username;
+
+                    header("Location: index.html");
+                } 
+                else {
+                    echo "Incorrect password";
+                }
+            }
+            else{
+                echo"No user found";
+            }
     }
 }
