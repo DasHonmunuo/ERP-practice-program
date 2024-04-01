6abcfa1658e258da961a9757ca9fe13c83728aae
DasHonmunuo <124122787+DasHonmunuo@users.noreply.github.com>
2024-04-01 12:01:48 +0800

    Added add empoyee button

---
+++ script.js
@@ -0,0 +1,9 @@
+// to make a div appear/disappear when clicking a button
+function toggleDiv(divId) {
+    var div = document.getElementById(divId);
+    if (div.style.display === "none") {
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
